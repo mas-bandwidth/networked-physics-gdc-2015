74c27606d35b87f3fff20ff0906df902c8f08edf
Glenn Fiedler <glenn.fiedler@gmail.com>
2014-07-06 21:12:53 -0700

    done

--- build/obj/x64/Debug/UnitTest/TestDNSResolver.d
+++
@@ -1,4 +0,0 @@
-obj/x64/Debug/UnitTest/TestDNSResolver.o: ../tests/TestDNSResolver.cpp \
-  ../include/Config.h
-
-../include/Config.h:
--- obj/x64/Debug/DNSResolver.d
+++
@@ -1,6 +0,0 @@
-obj/x64/Debug/DNSResolver.o: src/DNSResolver.cpp include/DNSResolver.h \
-  include/Config.h
-
-include/DNSResolver.h:
-
-include/Config.h:
