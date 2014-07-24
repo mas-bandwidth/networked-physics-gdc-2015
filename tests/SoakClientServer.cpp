e95a60ad926c232cea7b7dab565d4127e742137c
Glenn Fiedler <glenn.fiedler@gmail.com>
2014-07-24 00:09:38 -0700

    basic client server soak test

--- tests/SoakClientServer.cpp
+++ tests/SoakClientServer.cpp
@@ -1,64 +1,49 @@
 #include "Connection.h"
 #include "TestMessages.h"
 #include "TestPackets.h"
-#include "NetworkSimulator.h"
+#include "TestChannelStructure.h"
 #include "ReliableMessageChannel.h"
+#include "NetworkSimulator.h"
+#include "BSDSocket.h"
+#include "Client.h"
+#include "Server.h"
+#include "Network.h"
 #include <time.h>
+#include <chrono>
+#include <thread>
 
 using namespace protocol;
 
-class TestChannelStructure : public ChannelStructure
+struct ClientInfo
 {
-    ReliableMessageChannelConfig m_config;
+    Client * client;
+    Block * clientData;
+    NetworkInterface * networkInterface;
+};
 
-public:
-
-    TestChannelStructure( MessageFactory & messageFactory )
-        : ChannelStructure( memory::default_allocator(), memory::scratch_allocator(), 1 )
-    {
-        m_config.maxMessagesPerPacket = 256;
-        m_config.sendQueueSize = 2048;
-        m_config.receiveQueueSize = 512;
-        m_config.packetBudget = 4000;
-        m_config.maxMessageSize = 1024;
-        m_config.blockFragmentSize = 3900;
-        m_config.maxLargeBlockSize = 32 * 1024 * 1024;
-        m_config.messageFactory = &messageFactory;
-        m_config.messageAllocator = &memory::default_allocator();
-        m_config.smallBlockAllocator = &memory::default_allocator();
-        m_config.largeBlockAllocator = &memory::default_allocator();
-    }
-
-    const ReliableMessageChannelConfig & GetConfig() const
-    {
-        return m_config;
-    }
-
-protected:
-
-    const char * GetChannelNameInternal( int channelIndex ) const
-    {
-        return "reliable message channel";
-    }
-
-    Channel * CreateChannelInternal( int channelIndex )
-    {
-        return PROTOCOL_NEW( GetChannelAllocator(), ReliableMessageChannel, m_config );
-    }
-
-    ChannelData * CreateChannelDataInternal( int channelIndex )
-    {   
-        return PROTOCOL_NEW( GetChannelDataAllocator(), ReliableMessageChannelData, m_config );
-    }
+struct ServerInfo
+{
+    Address address;
+    Server * server;
+    Block * serverData;
+    NetworkInterface * networkInterface;
 };
 
 void soak_test()
 {
 #if PROFILE
-    printf( "[profile]\n" );
+    printf( "[profile client server]\n" );
 #else
-    printf( "[soak test]\n" );
+    printf( "[soak client server]\n" );
 #endif
+
+    // configu
+
+    const int NumServers = 8;
+    const int NumClients = 256;
+    const int NumClientsPerServer = 16;
+    const int BaseServerPort = 10000;
+    const int BaseClientPort = 20000;
 
     TestMessageFactory messageFactory( memory::default_allocator() );
 
@@ -66,181 +51,116 @@ void soak_test()
 
     TestPacketFactory packetFactory( memory::default_allocator(), &channelStructure );
 
-    const int MaxPacketSize = 4096;
+    // create a bunch of servers starting at port 1000
 
-#if !PROFILE
-    NetworkSimulatorConfig simulatorConfig;
-    simulatorConfig.packetFactory = &packetFactory;
-    NetworkSimulator simulator( simulatorConfig );
-    simulator.AddState( { 0.0f, 0.0f, 0.0f } );
-    simulator.AddState( { 0.0f, 0.0f, 0.0f } );
-    simulator.AddState( { 0.1f, 0.01f, 1.0f } );
-    simulator.AddState( { 0.1f, 0.01f, 1.0f } );
-    simulator.AddState( { 0.2f, 0.10f, 5.0f } );
-    simulator.AddState( { 0.3f, 0.20f, 10.0f } );
-    simulator.AddState( { 0.5f, 0.25f, 25.0f } );
-    simulator.AddState( { 1.0f, 0.50f, 50.0f } );
-    simulator.AddState( { 1.0f, 1.00f, 100.0f } );
-#endif
+    ServerInfo serverInfo[NumServers];
 
-    Address address( "::1" );
+    for( int i = 0; i < NumServers; ++i )
+    {
+        serverInfo[i].address = Address( "::1" );
+        serverInfo[i].address.SetPort( BaseServerPort + i );
 
-    ConnectionConfig connectionConfig;
-    connectionConfig.packetType = PACKET_CONNECTION;
-    connectionConfig.maxPacketSize = MaxPacketSize;
-    connectionConfig.packetFactory = &packetFactory;
-    connectionConfig.slidingWindowSize = 1024;
-    connectionConfig.channelStructure = &channelStructure;
+        BSDSocketConfig bsdSocketConfig;
+        bsdSocketConfig.port = BaseServerPort + i;
+        bsdSocketConfig.maxPacketSize = 1200;
+        bsdSocketConfig.packetFactory = &packetFactory;
+        serverInfo[i].networkInterface = PROTOCOL_NEW( memory::default_allocator(), BSDSocket, bsdSocketConfig );
 
-    Connection connection( connectionConfig );
+        const int serverDataSize = 10 + 256 * i + 11 + i;
+        serverInfo[i].serverData = PROTOCOL_NEW( memory::default_allocator(), Block, memory::default_allocator(), serverDataSize );
+        {
+            uint8_t * data = serverInfo[i].serverData->GetData();
+            for ( int j = 0; j < serverDataSize; ++j )
+                data[j] = ( 10 + i + j ) % 256;
+        }
 
-    auto messageChannel = static_cast<ReliableMessageChannel*>( connection.GetChannel( 0 ) );
+        ServerConfig serverConfig;
+        serverConfig.serverData = serverInfo[i].serverData;
+        serverConfig.maxClients = NumClientsPerServer;
+        serverConfig.channelStructure = &channelStructure;
+        serverConfig.networkInterface = serverInfo[i].networkInterface;
 
-    auto messageChannelConfig = channelStructure.GetConfig(); 
-    
-    uint16_t sendMessageId = 0;
-    uint64_t numMessagesSent = 0;
-    uint64_t numMessagesReceived = 0;
+        serverInfo[i].server = PROTOCOL_NEW( memory::default_allocator(), Server, serverConfig );
+    }
+
+    ClientInfo clientInfo[NumClients];
+
+    for( int i = 0; i < NumClients; ++i )
+    {
+        BSDSocketConfig bsdSocketConfig;
+        bsdSocketConfig.port = BaseClientPort + i;
+        bsdSocketConfig.maxPacketSize = 1200;
+        bsdSocketConfig.packetFactory = &packetFactory;
+        clientInfo[i].networkInterface = PROTOCOL_NEW( memory::default_allocator(), BSDSocket, bsdSocketConfig );
+
+        const int clientDataSize = 10 + 64 * i + 21 + i;
+        clientInfo[i].clientData = PROTOCOL_NEW( memory::default_allocator(), Block, memory::default_allocator(), clientDataSize );
+        {
+            uint8_t * data = clientInfo[i].clientData->GetData();
+            for ( int j = 0; j < clientDataSize; ++j )
+                data[j] = ( 20 + i + j ) % 256;
+        }
+
+        ClientConfig clientConfig;
+        clientConfig.clientData = clientInfo[i].clientData;
+        clientConfig.channelStructure = &channelStructure;
+        clientConfig.networkInterface = clientInfo[i].networkInterface;
+
+        clientInfo[i].client = PROTOCOL_NEW( memory::default_allocator(), Client, clientConfig );
+    }
 
     TimeBase timeBase;
-    timeBase.time = 0.0;
-    timeBase.deltaTime = 0.01;
+    timeBase.deltaTime = 1.0 / 60.0;
 
-//    for ( int i = 0; i < 10000; ++i )
     while ( true )
+    //for ( int iteration = 0; iteration < 1024; ++iteration )
     {
-        const int maxMessagesToSend = 1 + rand() % 32;
-
-        for ( int i = 0; i < maxMessagesToSend; ++i )
+        for ( int i = 0; i < NumServers; ++i )
         {
-            if ( !messageChannel->CanSendMessage() )
-                break;
-
-            int value = rand() % 10000;
-
-            if ( value < 5000 )
-            {
-                // bitpacked message
-                auto message = (TestMessage*) messageFactory.Create( MESSAGE_TEST );
-                PROTOCOL_CHECK( message );
-                message->sequence = sendMessageId;
-                messageChannel->SendMessage( message );
-            }
-            else if ( value < 9999 )
-            {
-                // small block
-                int index = sendMessageId % 32;
-                Block block( memory::default_allocator(), index + 1 );
-                uint8_t * data = block.GetData();
-                for ( int i = 0; i < block.GetSize(); ++i )
-                    data[i] = ( index + i ) % 256;
-                messageChannel->SendBlock( block );
-            }
-            else
-            {
-                // large block
-                int index = sendMessageId % 4;
-                Block block( memory::default_allocator(), (index+1) * 1024 * 1000 + index );
-                uint8_t * data = block.GetData();
-                for ( int i = 0; i < block.GetSize(); ++i )
-                    data[i] = ( index + i ) % 256;
-                messageChannel->SendBlock( block );
-            }
-            
-            sendMessageId++;
-            numMessagesSent++;
+            serverInfo[i].server->Update( timeBase );
         }
 
-        ConnectionPacket * writePacket = connection.WritePacket();
-
-        uint8_t buffer[MaxPacketSize];
-
-        WriteStream writeStream( buffer, MaxPacketSize );
-        writePacket->SerializeWrite( writeStream );
-        writeStream.Flush();
-        PROTOCOL_CHECK( !writeStream.IsOverflow() );
-
-        packetFactory.Destroy( writePacket );
-        writePacket = nullptr;
-
-        ReadStream readStream( buffer, MaxPacketSize );
-        auto readPacket = (ConnectionPacket*) packetFactory.Create( PACKET_CONNECTION );
-        readPacket->SerializeRead( readStream );
-        PROTOCOL_CHECK( !readStream.IsOverflow() );
-
-        connection.ReadPacket( static_cast<ConnectionPacket*>( readPacket ) );
-
-        packetFactory.Destroy( readPacket );
-        readPacket = nullptr;
-
-        connection.Update( timeBase );
-
-        while ( true )
+        for ( int i = 0; i < NumClients; ++i )
         {
-            auto message = messageChannel->ReceiveMessage();
+            clientInfo[i].client->Update( timeBase );
 
-            if ( !message )
-                break;
-
-            PROTOCOL_CHECK( message->GetId() == numMessagesReceived % 65536 );
-            PROTOCOL_CHECK( message->GetType() == MESSAGE_BLOCK || message->GetType() == MESSAGE_TEST );
-
-            if ( message->GetType() == MESSAGE_TEST )
+            if ( clientInfo[i].client->IsConnected() )
             {
-#if !PROFILE
-                printf( "%09.2f - received message %d - test message\n", timeBase.time, message->GetId() );
-#endif
-            }
-            else
-            {
-                auto blockMessage = static_cast<BlockMessage*>( message );
-                
-                Block & block = blockMessage->GetBlock();                
-
-                if ( block.GetSize() <= messageChannelConfig.maxSmallBlockSize )
+                if ( ( rand() % 100 ) == 0 )
                 {
-                    const int index = numMessagesReceived % 32;
-                    PROTOCOL_CHECK( block.GetSize() == index + 1 );
-                    const uint8_t * data = block.GetData();
-                    for ( int i = 0; i < block.GetSize(); ++i )
-                        PROTOCOL_CHECK( data[i] == ( index + i ) % 256 );
-#if !PROFILE
-                    printf( "%09.2f - received message %d - small block\n", timeBase.time, message->GetId() );
-#endif
-                }
-                else
-                {
-                    const int index = numMessagesReceived % 4;
-                    PROTOCOL_CHECK( block.GetSize() == ( index + 1 ) * 1024 * 1000 + index );
-                    const uint8_t * data = block.GetData();
-                    for ( int i = 0; i < block.GetSize(); ++i )
-                        PROTOCOL_CHECK( data[i] == ( index + i ) % 256 );
-#if !PROFILE
-                    printf( "%09.2f - received message %d - large block\n", timeBase.time, message->GetId() );
-#endif
+                    printf( "disconnect client %d\n", i );
+                    clientInfo[i].client->Disconnect();
                 }
             }
 
-            numMessagesReceived++;
-
-            messageFactory.Release( message );
+            if ( clientInfo[i].client->GetState() == CLIENT_STATE_DISCONNECTED )
+            {
+                if ( ( rand() % 200 ) == 0 )
+                {
+                    const int serverIndex = rand() % NumServers;
+                    printf( "connect client %d to server %d\n", i, serverIndex );
+                    clientInfo[i].client->Connect( serverInfo[serverIndex].address );                    
+                }
+            }
         }
 
-#if !PROFILE
-        auto status = messageChannel->GetReceiveLargeBlockStatus();
-        if ( status.receiving )
-            printf( "%09.2f - receiving large block %d - %d/%d fragments\n",
-                    timeBase.time, 
-                    status.blockId, 
-                    status.numReceivedFragments, 
-                    status.numFragments );
-#endif
-
-        PROTOCOL_CHECK( messageChannel->GetCounter( RELIABLE_MESSAGE_CHANNEL_COUNTER_MESSAGES_SENT ) == numMessagesSent );
-        PROTOCOL_CHECK( messageChannel->GetCounter( RELIABLE_MESSAGE_CHANNEL_COUNTER_MESSAGES_RECEIVED ) == numMessagesReceived );
-        PROTOCOL_CHECK( messageChannel->GetCounter( RELIABLE_MESSAGE_CHANNEL_COUNTER_MESSAGES_EARLY ) == 0 );
+        std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
 
         timeBase.time += timeBase.deltaTime;
+    }
+
+    for ( int i = 0; i < NumServers; ++i )
+    {
+        PROTOCOL_DELETE( memory::default_allocator(), Server, serverInfo[i].server );
+        PROTOCOL_DELETE( memory::default_allocator(), Block, serverInfo[i].serverData );
+        PROTOCOL_DELETE( memory::default_allocator(), NetworkInterface, serverInfo[i].networkInterface );
+    }
+
+    for ( int i = 0; i < NumClients; ++i )
+    {
+        PROTOCOL_DELETE( memory::default_allocator(), Client, clientInfo[i].client );
+        PROTOCOL_DELETE( memory::default_allocator(), Block, clientInfo[i].clientData );
+        PROTOCOL_DELETE( memory::default_allocator(), NetworkInterface, clientInfo[i].networkInterface );
     }
 }
 
@@ -250,7 +170,19 @@ int main()
 
     memory::initialize();
 
+    srand( time( nullptr ) );
+
+    if ( !InitializeNetwork() )
+    {
+        printf( "failed to initialize network\n" );
+        return 1;
+    }
+
+    PROTOCOL_ASSERT( IsNetworkInitialized() );
+
     soak_test();
+
+    ShutdownNetwork();
 
     memory::shutdown();
 
