dc56e958fdefe0172f383fc08c690dc4c0645013
Glenn Fiedler <glenn.fiedler@gmail.com>
2014-10-18 19:26:36 -0700

    wip on cubemap

--- data/shaders/Stone.frag
+++ data/shaders/Stone.frag
@@ -7,11 +7,13 @@ uniform vec4 LightPosition;
 uniform vec3 LightIntensity = vec3( 1, 1, 1 );
 uniform vec3 Ks = vec3( 0.5, 0.5, 0.5 );
 uniform vec3 Kd = vec3( 0.5, 0.5, 0.5 );
-uniform vec3 Ld = vec3( 1.0, 1.0, 1.0 );
-uniform vec3 Ka = vec3( 0.4, 0.4, 0.4 );
+uniform vec3 Ld = vec3( 0.5, 0.5, 0.5 );
+uniform vec3 Ka = vec3( 0.2, 0.2, 0.2 );
 uniform float Shininess = 50.0;
 
 layout( location = 0 ) out vec4 FragColor;
+
+uniform samplerCube CubeMap;
 
 void main()
 {
@@ -25,5 +27,7 @@ void main()
 
     vec3 r = reflect( -s, n );
 
-    FragColor = vec4( LightIntensity * ( Ka + Kd * max( dot(s,n), 0.0 ) + Ks * pow( max( dot(r,v), 0.0 ), Shininess ) ), 1 );
+    vec4 cubemap_color = texture( CubeMap, reflect( v, n ) );
+
+    FragColor = cubemap_color * vec4( LightIntensity * ( Ka + Kd * max( dot(s,n), 0.0 ) + Ks * pow( max( dot(r,v), 0.0 ), Shininess ) ), 1 );
 }
--- src/game/StoneDemo.cpp
+++ src/game/StoneDemo.cpp
@@ -4,12 +4,16 @@
 
 #include "Global.h"
 #include "Mesh.h"
+#include "Render.h"
 #include "MeshManager.h"
 #include "StoneManager.h"
 #include "ShaderManager.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/gtc/matrix_transform.hpp>
+#include "nv_dds.h"
+
+using namespace nv_dds;
 
 const int NumStoneSizes = 14;
 
@@ -65,6 +69,8 @@ static bool stoneColor = WHITE;
 static int stoneSize = NumStoneSizes - 1;
 static int stoneMode = SINGLE_STONE;
 
+static GLuint cubemap_texture = 0;
+
 StoneDemo::StoneDemo()
 {
     // ...
@@ -72,11 +78,68 @@ StoneDemo::StoneDemo()
 
 StoneDemo::~StoneDemo()
 {
-    // ...
+    if ( cubemap_texture )
+    {
+        glDeleteTextures( 1, &cubemap_texture );
+        cubemap_texture = 0;
+    }
 }
 
 bool StoneDemo::Initialize()
 {
+    const char * filename = "data/cubemaps/Output.dds";
+
+    // todo: check if the filename doesn't exist. if not don't try to load it
+
+    CDDSImage image;
+    image.load( filename );    
+    if ( !image.is_valid() || !image.is_cubemap() )
+    {
+        printf( "%.3f: error - cubemap image \"%s\" is not valid, or is not a cubemap\n", global.timeBase.time, filename );
+        return false;
+    }
+
+    /*
+    printf( "is_compressed = %d\n", image.is_compressed() );
+    printf( "is_cubemap = %d\n", image.is_cubemap() );
+    printf( "is_valid = %d\n", image.is_valid() );
+    printf( "width = %d\n", image.get_width() );
+    printf( "height = %d\n", image.get_height() );
+    printf( "depth = %d\n", image.get_depth() );
+    printf( "num_mipmaps = %d\n", image.get_num_mipmaps() );
+    */
+
+    glGenTextures( 1, &cubemap_texture );
+
+    glEnable( GL_TEXTURE_CUBE_MAP_SEAMLESS );
+    
+    glBindTexture( GL_TEXTURE_CUBE_MAP, cubemap_texture );
+
+    for ( int n = 0; n < 6; n++ )
+    {
+        int target = GL_TEXTURE_CUBE_MAP_POSITIVE_X + n;
+
+        auto & cubemap_face = image.get_cubemap_face( n );
+
+        const int num_mipmaps = cubemap_face.get_num_mipmaps();
+
+        for ( int i = 0; i <= num_mipmaps; ++i )
+        {
+            const int format = GL_RGBA;
+            const int width = (i==0) ? cubemap_face.get_width() : image.get_mipmap(i-1).get_width();
+            const int height = (i==0) ? cubemap_face.get_height() : image.get_mipmap(i-1).get_height();
+            const uint8_t * data = (i==0) ? cubemap_face : cubemap_face.get_mipmap(i-1);
+            glTexImage2D( target, i, format, width, height, 0, format, GL_UNSIGNED_BYTE, data );
+        }
+    }
+
+    glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
+    glTexParameteri( GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+    glBindTexture( GL_TEXTURE_CUBE_MAP, 0 );
+
+    check_opengl_error( "after load cubemap dds" );
+
     return true;
 }
 
@@ -106,6 +169,14 @@ void StoneDemo::Render()
         return;
 
     glUseProgram( shader );
+
+    glActiveTexture( GL_TEXTURE0 );
+
+    glBindTexture( GL_TEXTURE_CUBE_MAP, cubemap_texture );
+
+    int location = glGetUniformLocation( shader, "CubeMap" );
+    if ( location >= 0 )
+        glUniform1i( location, 0 );
 
     switch ( stoneMode )
     {
@@ -176,6 +247,8 @@ void StoneDemo::Render()
         }
         break;
     }
+
+    glBindTexture( GL_TEXTURE_CUBE_MAP, 0 );
 
     glUseProgram( 0 );
 }
