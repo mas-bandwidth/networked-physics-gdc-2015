f2d61bf975294caeb3c15b4a6c2acefd558a6bb0
Glenn Fiedler <glenn.fiedler@gmail.com>
2015-01-03 23:20:03 -0800

    finished recording videos for snapshots and interpolation article pt 1

--- src/game/SnapshotDemo.cpp
+++ src/game/SnapshotDemo.cpp
@@ -525,6 +525,7 @@ struct SnapshotInternal
 SnapshotDemo::SnapshotDemo( core::Allocator & allocator )
 {
     InitSnapshotModes();
+//    SetMode( SNAPSHOT_MODE_HERMITE_EXTRAPOLATION_10PPS );
     m_allocator = &allocator;
     m_internal = nullptr;
     m_settings = CORE_NEW( *m_allocator, CubesSettings );
